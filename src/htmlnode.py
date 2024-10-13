456877fbd25985cd290b7949ca821d01dedda117
dave long <davelongdev@gmail.com>
2024-10-12 21:33:12 -0400

    add draft of parent node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,3 +36,24 @@ class LeafNode(HTMLNode):
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("parent node must have a tag")
+        if self.children is None:
+            raise ValueError("parent node must have children")
+        else:
+            props = self.props_to_html()
+            children = ''
+            for child in self.children:
+                child = child.to_html()
+                children += child
+            string = f'<{self.tag}{props}{children}</{self.tag}>'
+            return string
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, children: {self.children}, {self.props})"
+
